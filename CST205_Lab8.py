264571902024293a51161bcef058cd77f3787404
grace289 <33207084+grace289@users.noreply.github.com>
2017-11-19 19:16:36 -0800

    Create CST205_Lab8.py

---
+++ CST205_Lab8.py
@@ -0,0 +1,50 @@
+
+#Lab 8
+def increaseVolume(sound):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value*2)
+  return sound
+    
+def decreaseVolume(sound):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value/2)
+  return sound
+    
+def changeVolume(sound, factor):
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    setSampleValue(sample, value * factor)
+  return sound
+    
+def maxSample(sound):
+  maxValue = 0
+  for sample in getSamples(sound):
+    value = getSampleValue(sample)
+    maxValue = max(maxValue, value)
+  return maxValue
+
+def maxVolume(sound):
+    factor = 32767 / maxSample(sound)
+    for sample in getSamples(sound):
+        value = getSampleValue(sample)
+        setSampleValue(sample, value * factor)
+    return sound
+
+def goToEleven(sound):
+    for sample in range(0, getLength(sound)):
+        if getSampleValueAt(sound, sample) > 0:
+            setSampleValueAt(sound, sample, 32767)
+        elif getSampleValueAt(sound, sample) < 0:
+            setSampleValueAt(sound, sample, -32768)
+    return sound
+
+
+#Call Function                                                                                                 
+s = pickAFile()
+sd = makeSound(s)
+#enter function here
+#example goToEleven(sd)
+play(sd)
+explore(sd)
